0c8f05dae3f632b18412537b1277712335c7636b
Tegan Rauh <onlysayinghi@gmail.com>
2021-05-04 21:34:57 -0500

    Remove karma and add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+};
