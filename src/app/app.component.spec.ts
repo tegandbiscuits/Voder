1a595c60d3a9e785db670de39d27d2e88cc79330
Tegan Rauh <onlysayinghi@gmail.com>
2021-05-11 19:59:51 -0500

    Squashed commit of the following:
    commit 61c89e5f3aa916e5148c85365045edd885283946
    Author: Tegan Rauh <onlysayinghi@gmail.com>
    Date:   Thu May 6 17:21:10 2021 -0500
    
        Passing e2e test
    
    commit c2b059a38f1c1ce2559bf8b7a54eedbd540b09b3
    Author: Tegan Rauh <onlysayinghi@gmail.com>
    Date:   Thu May 6 17:17:39 2021 -0500
    
        Fix tests
    
    commit e1092e4b016261533f57d8e58134bbfecbe6d924
    Author: Tegan Rauh <onlysayinghi@gmail.com>
    Date:   Thu May 6 17:15:34 2021 -0500
    
        Fix lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlayerComponent } from './player/player.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, HeaderComponent, PlayerComponent],
     }).compileComponents();
   });
 
@@ -20,14 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('podcast-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'podcast-app app is running!'
-    );
   });
 });
