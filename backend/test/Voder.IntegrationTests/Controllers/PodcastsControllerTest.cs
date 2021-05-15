457105e5bd91b0a4b8c10e64be3e05e9f27ca738
Tegan Rauh <onlysayinghi@gmail.com>
2021-05-14 21:25:40 -0500

    Add rest of tests
    They're flakey and repetative, but I shall fix that.

--- backend/test/Voder.IntegrationTests/Controllers/PodcastsControllerTest.cs
+++ backend/test/Voder.IntegrationTests/Controllers/PodcastsControllerTest.cs
@@ -7,6 +7,8 @@ using Voder.Models;
 using Microsoft.Extensions.DependencyInjection;
 using AutoFixture;
 using System.Linq;
+using System.Net;
+using System.Text;
 
 namespace Voder.IntegrationTests.Controllers
 {
@@ -28,10 +30,12 @@ namespace Voder.IntegrationTests.Controllers
         }
 
         [Fact]
-        public async Task CanGetPodcasts()
+        [Trait("PodcastsController", "Index")]
+        public async Task GetsThePodcasts()
         {
             var testPodcast = this.fixture.Create<Podcast>();
             this.dbContext.Podcasts.Add(testPodcast);
+            this.dbContext.Podcasts.Add(this.fixture.Create<Podcast>());
             await this.dbContext.SaveChangesAsync();
 
             var httpResponse = await this.client.GetAsync("/Podcasts");
@@ -40,8 +44,65 @@ namespace Voder.IntegrationTests.Controllers
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
             var podcasts = JsonConvert.DeserializeObject<IEnumerable<Podcast>>(stringResponse);
 
-            Assert.Single(podcasts);
+            Assert.Equal(2, podcasts.Count());
             Assert.Contains(podcasts, p => p.FeedUrl == testPodcast.FeedUrl);
+        }
+
+        [Fact]
+        [Trait("PodcastsController", "Show")]
+        public async Task ReturnsThePodcastWhenItExists()
+        {
+            var testPodcast = this.fixture.Create<Podcast>();
+            this.dbContext.Podcasts.Add(testPodcast);
+            await this.dbContext.SaveChangesAsync();
+
+            var httpResponse = await this.client.GetAsync($"/Podcasts/{testPodcast.Id}");
+            httpResponse.EnsureSuccessStatusCode();
+
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var returnedPodcast = JsonConvert.DeserializeObject<Podcast>(stringResponse);
+
+            Assert.Equal(testPodcast.Id, returnedPodcast.Id);
+        }
+
+        [Fact]
+        [Trait("PodcastsController", "Show")]
+        public async Task ReturnsAnErrorWhenThePodcastDoesNotExist()
+        {
+            var httpResponse = await this.client.GetAsync("/Podcasts/123");
+            Assert.Equal(HttpStatusCode.NotFound, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        [Trait("PodcastsController", "Create")]
+        public async Task CreatesAPodcast()
+        {
+            var podcastCount = this.dbContext.Podcasts.Count();
+
+            var body = new { FeedUrl = "http://test.com" };
+            var httpContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+            var httpResponse = await this.client.PostAsync("/Podcasts", httpContent);
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.Equal(podcastCount + 1, this.dbContext.Podcasts.Count());
+        }
+
+        [Fact]
+        [Trait("PodcastsController", "Removal")]
+        public async Task DeletesThePodcast()
+        {
+            var testPodcast = this.fixture.Create<Podcast>();
+            this.dbContext.Podcasts.Add(testPodcast);
+            await this.dbContext.SaveChangesAsync();
+
+            var deleteResposne = await this.client.DeleteAsync($"/Podcasts/{testPodcast.Id}");
+            deleteResposne.EnsureSuccessStatusCode();
+
+            var indexResponse = await this.client.GetAsync("/Podcasts");
+            indexResponse.EnsureSuccessStatusCode();
+            var stringResponse = await indexResponse.Content.ReadAsStringAsync();
+            var podcasts = JsonConvert.DeserializeObject<IEnumerable<Podcast>>(stringResponse);
+            Assert.DoesNotContain(podcasts, p => p.Id == testPodcast.Id);
         }
     }
 }
